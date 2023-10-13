67503c67998cad11068f2473c0ddbe3450f3c4d5
Jamie Jones <jamie@mudslidedesign.co.uk>
2023-10-13 07:37:40 +0100

    adding canTest script

---
+++ canTest.py
@@ -0,0 +1,26 @@
+import can
+import time
+
+startTime_ms = round(time.time()*1000)
+def addToTrace(s):
+    currentTime_ms = round(time.time()*1000)
+    dT_ms = currentTime_ms - startTime_ms
+    print("[" + str(dT_ms) + "ms] " + s)
+
+filters = [
+    {"can_id": 0x355, "can_mask": 0x7FF, "extended": False},
+    {"can_id": 0x356, "can_mask": 0x7FF, "extended": False}]
+canbus = can.interface.Bus(bustype='socketcan', channel="can0", can_filters = filters)
+
+while(1):
+    message = canbus.recv(0)
+    if message:
+        #simpbms SoC
+        if message.arbitration_id == 0x355:
+            soc = (message.data[1] << 8) + message.data[0]
+            addToTrace("PI: Set capacity to %d" % soc)
+
+        #simbms Voltage
+        if message.arbitration_id == 0x356:
+            vtg = ((message.data[1]<< 8) + message.data[0]) / 100
+            addToTrace("PI: Set battery voltage to %d V" % vtg)
